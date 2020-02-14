de5fefc5621f979c85f363297127ef2b925f387e
Andrew Philpott <andrewphilpott92@gmail.com>
2020-02-14 09:17:31 -0800

    Add doctorService.js, interface-logic.js, and geocodeService.js

---
+++ src/js/doctorService.js
@@ -0,0 +1,13 @@
+export class DoctorService {
+  constructor() {
+
+  }
+
+  async asyncDoctorsForCityCall() {
+    try {
+      const response = await fetch(``)
+    } catch (error) {
+      
+    }
+  }
+}
---
+++ src/js/geocodeService.js
@@ -0,0 +1,13 @@
+export class GeocodeService {
+  constructor() {
+
+  }
+
+  async asyncCoordinatesForCity() {
+  try {
+    const response = await fetch(``);
+  } catch (error) {
+    
+  }
+  }
+}
